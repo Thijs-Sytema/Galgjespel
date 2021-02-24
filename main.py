0d38992fadee8106fea847bae1f925a95a497dbc
Thijs Sytema <110443@st-maartenscollege.nl>
2021-02-24 09:41:25 +0000

    Eerste functie toegevoegd, deze functie kiest een woord uit de woordenlijst

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 word_list = ["informatica","informatiekunde","spelletje","aardigheidje","scholier","fotografie","waardebepaling","specialtieit","verzekering","universiteit","heesterperk"]
+
+def get word(word_list):
+  word = random.choice(word_list)
+  return word.upper()
